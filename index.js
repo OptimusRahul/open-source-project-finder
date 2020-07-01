c5b34b87d78e9dbe2075b7dfd46e0ec8bff1a994
OptimusRahul <rockerahul97@gmail.com>
2020-07-01 23:10:22 +0530

    modified index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const 
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res)=>{

de4b34fef654a006f6088e6bda95bcfde82ad36e
OptimusRahul <rockerahul97@gmail.com>
2020-07-01 23:06:40 +0530

    modified index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const 
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res)=>{
     res.render('index.html')
 })
 
-app.listen(8080, console.log('running at 8080'));
+app.listen(process.env.PORT || 8080, console.log('running at 8080'));
